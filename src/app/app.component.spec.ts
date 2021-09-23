55c2b70baf9537f68d6cd2439206eb4a106292de
Nicholas Brown <sneakylunatic69@gmail.com>
2021-09-22 20:13:55 -0700

    Create CI.yml (#1)
    * Create CI.yml
    
    * update CI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'cosc414-project1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cosc414-project1');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cosc414-project1 app is running!');
-  });
 });
