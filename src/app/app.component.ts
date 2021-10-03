efd6f246669282324db43bff0bc7690ce81c05e4
Nicholas Brown <sneakylunatic69@gmail.com>
2021-10-02 20:18:14 -0700

    Improved Website UI (#5)
    * header component
    
    * looks nice
    
    * linting check
    
    * wording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cosc414-project1';
+  title = 'COSC 414 - Project 1';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'github',
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/GitHub-Mark.svg")
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SceneComponent } from './scene/scene.component';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import { FooterComponent } from './footer/footer.component';
+import { ScoreComponent } from './score/score.component';
+import { InfoComponent } from './info/info.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SceneComponent
+    SceneComponent,
+    HeaderComponent,
+    FooterComponent,
+    ScoreComponent,
+    InfoComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatTabsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
