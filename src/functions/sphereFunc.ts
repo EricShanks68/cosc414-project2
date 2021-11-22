4193405d5d55232cd38d13949ec5cd4438be152f
EricShanks68 <e-ricshanks@hotmail.com>
2021-11-21 19:12:24 -0800

    up to date

--- src/functions/sphereFunc.ts
+++ src/functions/sphereFunc.ts
@@ -28,18 +28,21 @@ export function getDistanceBetweenTwoSpheres (s1: Sphere, s2: Sphere): number{
 
 
 export function smallerMoveIntoBigger (s1: Sphere, s2: Sphere, distance: number): void{
-  const distancePart = distance / 5;
+  const testNum = 20;
+  const distancePart = distance / testNum;
   if(s1.radius < s2.radius){
     //A_moved = A + |(B-A)|*d
     s1.location.x = s1.location.x + Math.abs(s2.location.x - s1.location.x) * distancePart;
     s1.location.y = s1.location.y + Math.abs(s2.location.y - s1.location.y) * distancePart;
     s1.location.z = s1.location.z + Math.abs(s2.location.z - s1.location.z) * distancePart;
-    s2.radius + s1.radius/5;
+    s2.radius + s1.radius/testNum;
+    s1.radius - s1.radius/testNum;
   } else{
     s2.location.x = s2.location.x + Math.abs(s1.location.x - s2.location.x) * distancePart;
     s2.location.y = s2.location.y + Math.abs(s1.location.y - s2.location.y) * distancePart;
     s2.location.z = s2.location.z + Math.abs(s1.location.z - s2.location.z) * distancePart;
-    s1.radius + s2.radius/5;
+    s1.radius + s2.radius/testNum;
+    s2.radius - s2.radius/testNum;
   }
  
 }
